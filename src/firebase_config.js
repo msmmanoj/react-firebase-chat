26c361917ba708fe241d00154c0de16a96bc5d91
manoj.malepati <manoj.malepati@ggktech.com>
2021-04-09 00:08:10 +0530

    retrieve secrets from github actions

--- src/firebase_config.js
+++ src/firebase_config.js
@@ -1,14 +1,16 @@
 import firebase from 'firebase/app';
 
 const config = {
-    apiKey: "AIzaSyDgFG6npILcyixEjb-MUjuf5tBrw2dUTzg",
-    authDomain: "react-firebase-chat-a7221.firebaseapp.com",
-    projectId: "react-firebase-chat-a7221",
-    storageBucket: "react-firebase-chat-a7221.appspot.com",
-    messagingSenderId: "356316568421",
-    appId: "1:356316568421:web:d41e35bf3ff96b8cdcdf8c",
-    measurementId: "G-M860FG15VC"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASURMENT_ID
 }
+
+console.log(config)
 
 !firebase.apps.length ? firebase.initializeApp(config) : firebase.app();
 
